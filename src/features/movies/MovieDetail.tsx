b8e383f3074d6b4ae4c6022a796d177e7e47f270
rstulug <rstulug@gmail.com>
2024-02-24 22:58:34 +0300

    Small changes

--- src/features/movies/MovieDetail.tsx
+++ src/features/movies/MovieDetail.tsx
@@ -7,11 +7,18 @@ export default function MovieDetail() {
   console.log(movieDetail);
   if (isLoading) return <Spinner />;
   return (
-    <div
-      style={{
-        backgroundImage: `linear-gradient(rgba(181, 192, 194, 0.438), rgba(54, 54, 61, 0.788)),url(${IMAGE_URL}${movieDetail.backdrop_path})`,
-      }}
-      className="w-full md:bg-cover h-[40rem]  "
-    ></div>
+    <div className="flex justify-between">
+      <div></div>
+      <div
+        style={{
+          backgroundImage: `linear-gradient(rgba(178, 204, 209, 0.623), rgba(113, 113, 136, 0.849)),url(${IMAGE_URL}${movieDetail.backdrop_path})`,
+        }}
+        className="w-[70%] h-[30rem] bg-no-repeat bg-cover "
+      >
+        <div className="text-zinc-200 flex justify-center items-end h-full py-2 px-2 font-semibold">
+          {movieDetail.overview}
+        </div>
+      </div>
+    </div>
   );
 }
