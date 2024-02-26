7fe5ed9e43c0a699bed17b896829a3effa91b06c
rstulug <rstulug@gmail.com>
2024-02-26 14:15:17 +0300

    deleted

--- src/features/movies/MovieDetail.tsx
+++ src/features/movies/MovieDetail.tsx
@@ -7,6 +7,7 @@ export default function MovieDetail() {
   console.log(movieDetail);
   if (isLoading) return <Spinner />;
   return (
+<<<<<<< HEAD
     <div className="flex h-[30rem] ml-8 mt-8 gap-5 text-sky-100">
       <div className="">
         <img
@@ -26,6 +27,18 @@ export default function MovieDetail() {
           {movieDetail.genres.map((genre: { id: number; name: string }) => (
             <span>{genre.name}</span>
           ))}
+=======
+    <div className="flex justify-between">
+      <div></div>
+      <div
+        style={{
+          backgroundImage: `linear-gradient(rgba(178, 204, 209, 0.623), rgba(113, 113, 136, 0.849)),url(${IMAGE_URL}${movieDetail.backdrop_path})`,
+        }}
+        className="w-[70%] h-[30rem] bg-no-repeat bg-cover "
+      >
+        <div className="text-zinc-200 flex justify-center items-end h-full py-2 px-2 font-semibold">
+          {movieDetail.overview}
+>>>>>>> parent of defaf5c (Small changes in moviedetail)
         </div>
       </div>
     </div>
