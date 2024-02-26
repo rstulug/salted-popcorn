2b457f7eaada26589c646eaa812b0315ca04b909
rstulug <rstulug@gmail.com>
2024-02-26 14:31:41 +0300

    Detail working

--- src/features/movies/MovieDetail.tsx
+++ src/features/movies/MovieDetail.tsx
@@ -7,16 +7,24 @@ export default function MovieDetail() {
   console.log(movieDetail);
   if (isLoading) return <Spinner />;
   return (
-    <div className="flex justify-between">
-      <div></div>
-      <div
-        style={{
-          backgroundImage: `linear-gradient(rgba(178, 204, 209, 0.623), rgba(113, 113, 136, 0.849)),url(${IMAGE_URL}${movieDetail.backdrop_path})`,
-        }}
-        className="w-[70%] h-[30rem] bg-no-repeat bg-cover "
-      >
-        <div className="text-zinc-200 flex justify-center items-end h-full py-2 px-2 font-semibold">
-          {movieDetail.overview}
+    <div className="flex ">
+      <div className="flex justify-between">
+        <div className="flex justify-center items-center">
+          <img
+            src={`${IMAGE_URL}${movieDetail.poster_path}`}
+            alt={movieDetail.title}
+            className="w-full h-[25rem] object-contain"
+          />
+        </div>
+        <div
+          style={{
+            backgroundImage: `linear-gradient(rgba(178, 204, 209, 0.623), rgba(113, 113, 136, 0.849)),url(${IMAGE_URL}${movieDetail.backdrop_path})`,
+          }}
+          className="w-[70%] h-[30rem] bg-no-repeat bg-cover "
+        >
+          <div className="text-zinc-200 flex justify-center items-end h-full py-2 px-2 font-semibold">
+            {movieDetail.overview}
+          </div>
         </div>
       </div>
     </div>
