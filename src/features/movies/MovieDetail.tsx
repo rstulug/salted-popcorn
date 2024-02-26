b125507739fe95f9b45ecf165f71a078828b569d
rstulug <rstulug@gmail.com>
2024-02-26 13:44:42 +0300

    small changes

--- src/features/movies/MovieDetail.tsx
+++ src/features/movies/MovieDetail.tsx
@@ -1,4 +1,3 @@
-import MovieItem from "../../ui/MovieItem";
 import Spinner from "../../ui/Spinner";
 import { IMAGE_URL } from "../../utils/constant";
 import { useMovie } from "./useMovie";
@@ -8,27 +7,25 @@ export default function MovieDetail() {
   console.log(movieDetail);
   if (isLoading) return <Spinner />;
   return (
-    <div
-      className="flex "
-      style={{
-        backgroundImage: `linear-gradient(rgba(178, 204, 209, 0.623), rgba(113, 113, 136, 0.849))`,
-      }}
-    >
-      <div className="w-[30%] h-[30rem] flex items-center">
+    <div className="flex h-[30rem] ml-8 mt-8 gap-5 text-sky-100">
+      <div className="">
         <img
           src={IMAGE_URL + movieDetail.poster_path}
           alt={`${movieDetail.title}`}
-          className="w-full h-[25rem] object-contain"
+          className="w-full h-[25rem] object-contain rounded-2xl"
         />
       </div>
-      <div
-        style={{
-          backgroundImage: `linear-gradient(rgba(178, 204, 209, 0.623), rgba(113, 113, 136, 0.849)),url(${IMAGE_URL}${movieDetail.backdrop_path})`,
-        }}
-        className="w-[70%] h-[30rem] bg-no-repeat bg-cover m-0"
-      >
-        <div className="text-gray-300 flex justify-center items-end h-full py-2 px-2 font-semibold text-center">
-          {movieDetail.overview}
+      <div>
+        <div className="font-bold text-2xl italic text-center">
+          {movieDetail.original_title
+            ? movieDetail.original_title
+            : movieDetail.title}
+        </div>
+        <div className="flex gap-3 ">
+          Genre:{" "}
+          {movieDetail.genres.map((genre: { id: number; name: string }) => (
+            <span>{genre.name}</span>
+          ))}
         </div>
       </div>
     </div>
