72285f03ed3705d2a2a4f9970757379ee824ede8
rstulug <rstulug@gmail.com>
2024-03-03 14:38:03 +0300

    Small changes

--- src/ui/SliderItem.tsx
+++ src/ui/SliderItem.tsx
@@ -11,35 +11,43 @@ interface ChildrenProp {
 export default function SliderItem({ data, type }: ChildrenProp) {
   const settings = {
     dots: false,
-    infinite: true,
-    speed: 1200,
-    slidesToShow: 6,
-    slidesToScroll: 6,
+    infinite: data.length < 2 ? false : true,
+    speed: 600,
+    slidesToShow: data.length < 6 ? data.length : 6,
+    slidesToScroll: data.length > 6 ? 1 : 0,
+    arrows: data.length < 6 ? false : true,
+    className: "index.css",
+
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
+          slidesToShow: data.length < 4 ? data.length : 4,
+          slidesToScroll: data.length > 4 ? 1 : 0,
+          arrows: data.length < 4 ? false : true,
+
+          infinite: data.length < 2 ? false : true,
           dots: false,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
+          slidesToShow: data.length < 2 ? data.length : 2,
+          slidesToScroll: data.length > 2 ? 1 : 0,
+          arrows: data.length < 2 ? false : true,
+          infinite: data.length < 2 ? false : true,
           dots: false,
         },
       },
+
       {
-        breakpoint: 480,
+        breakpoint: 375,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
+          slidesToScroll: data.length > 1 ? 1 : 0,
+          arrows: data.length < 1 ? false : true,
+          infinite: data.length < 2 ? false : true,
           dots: false,
         },
       },
@@ -56,7 +64,7 @@ export default function SliderItem({ data, type }: ChildrenProp) {
       </div>
     );
 
-  if (type === "cast")
+  if (type === "cast") {
     return (
       <div className="slider-container ">
         <Slider {...settings}>
@@ -66,6 +74,7 @@ export default function SliderItem({ data, type }: ChildrenProp) {
         </Slider>
       </div>
     );
+  }
 
   if (type === "tvshow")
     return (
