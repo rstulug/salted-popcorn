c7f542c6bd4d3a5a4c45d70f531fb53759a81513
rstulug <rstulug@gmail.com>
2024-03-20 11:39:41 +0300

    Spinner Style

--- src/ui/Spinner.tsx
+++ src/ui/Spinner.tsx
@@ -1,14 +1,10 @@
 function Spinner() {
   return (
-    <div className="flex justify-center items-center w-full">
+    <div className="flex justify-center items-center w-full h-96">
       <div
-        className={`inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]`}
+        className={`inline-block w-16 animate-spin rounded-full border-8 border-solid border-white border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] h-16`}
         role="status"
-      >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
-      </div>
+      ></div>
     </div>
   );
 }
