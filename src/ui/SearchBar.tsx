1ea2b0327aa39d88aa1c554b0942f32048eda6ff
rstulug <rstulug@gmail.com>
2024-03-30 00:23:35 +0300

    outside click and esc added

--- src/ui/SearchBar.tsx
+++ src/ui/SearchBar.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaClapperboard, FaPersonHalfDress, FaTv } from "react-icons/fa6";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 export default function SearchBar({
   setShowSearch,
@@ -13,6 +14,9 @@ export default function SearchBar({
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
 
+  //Detect outside click and clean search bar
+  const ref = useOutsideClick(() => setSearchQuery(""));
+
   function handleSearh(to: string) {
     navigate(`/search/${to}`);
     searchParams.set("search", searchQuery);
@@ -21,19 +25,34 @@ export default function SearchBar({
     setSearchQuery("");
     setShowSearch(false);
   }
+
+  //Detect Esc key and clean search bar
+  useEffect(function () {
+    function close(e: KeyboardEvent) {
+      if (e.key === "Escape") {
+        setSearchQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", close);
+
+    return () => document.removeEventListener("keydown", close);
+  }, []);
+
   return (
-    <div>
+    <div ref={ref}>
       <input
         type="text"
         placeholder="Search for movie, tv show or person"
         className="rounded-xl bg-gray-200 outline-none text-black placeholder:pl-6 pl-4 w-full placeholder:text-gray-500 transition-all "
         style={{ height: `${height}` }}
+        value={searchQuery}
         onChange={(e) => {
           setSearchQuery(e.target.value);
         }}
       />
       {searchQuery && (
-        <ul className="absolute top-6 w-full left-0 bg-gray-200 z-50 text-black">
+        <ul className="absolute top-8 w-full left-0 bg-gray-200 z-50 text-black rounded-t-md">
           <li
             onClick={() => handleSearh("movies")}
             className="py-2 pl-2 hover:bg-gray-400 rounded-lg cursor-pointer flex flex-row items-center gap-2"
